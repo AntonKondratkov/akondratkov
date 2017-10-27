2463b4875ecf571edb00bb45b124b00e7b901511
AntonKondratkov <mailto:kondratkov.anton@yandex.ru>
2017-10-27 10:09:00 +0700

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,14 @@
 package ru.job4j;
 
+/**
+*Class Calculate - решение задачи в части 001 Базовый Синтаксис, урок 1 JVM. Byte code.
+*@author Anton Kondratkov
+*@since 27.10.2017
+*/
 public class Calculate{
+	/**
+	* Метод main - метод с которой начинает выполнятся программа
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello world");
 	}
