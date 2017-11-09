81fdff90044b32019f5200011652603c823c6aec
AntonKondratkov <mailto:kondratkov.anton@yandex.ru>
2017-11-09 07:43:14 +0700

    1.1. Создание Hello World [#141]

--- chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -4,38 +4,53 @@ import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-
+/**
+ *@author Anton Kondratkov
+ *@since 4.11.2017
+ */
 public class CalculatorTest {
     @Test
     public void whenAddOnePlusOneThenTwo() {
         Calculator calc = new Calculator();
+        //act block
         calc.add(1D, 1D);
+        //assign block
         double result = calc.getResult();
         double expected = 2D;
+        //assert block
         assertThat(result, is(expected));
     }
     @Test
     public void whenMultiplyTwoMultiplyTwoThenFour() {
         Calculator calc = new Calculator();
+        //act block
         calc.multiple(2D, 2D);
+        //assign block
         double result = calc.getResult();
         double expected = 4D;
+        //assert block
         assertThat(result, is(expected));
     }
     @Test
     public void whenSubThreeMinusTwoThenOne() {
         Calculator calc = new Calculator();
+        //act block
         calc.sub(3D, 2D);
+        //assign block
         double result = calc.getResult();
         double expected = 1D;
+        //assert block
         assertThat(result, is(expected));
     }
     @Test
     public void whenDivSixDivTwoThenThree() {
         Calculator calc = new Calculator();
+        //act block
         calc.div(6D, 2D);
+        //assign block
         double result = calc.getResult();
         double expected = 3D;
+        //assert block
         assertThat(result, is(expected));
     }
 }
