7a6646908e21d08f11f3a8dd19e773c0dd410d9b
AntonKondratkov <mailto:kondratkov.anton@yandex.ru>
2017-11-09 10:53:04 +0700

    3.1. Максимум из двух чисел [#189]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,15 @@
+package ru.job4j.max;
+/**
+ *Class Max - решение задачи в части 001 Базовый Синтаксис, урок 4 Условный оператор.
+ *@author Anton Kondratkov
+ *@since 9.11.2017
+ */
+public class Max {
+    /**
+     *Method max вычисляет максимум из двух чисел.
+     *@return result.
+     */
+    public int max(int first, int second) {
+        return first > second ? first : second;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.max;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ *@author Anton Kondratkov
+ *@since 9.11.2017
+ */
+
+public class MaxTest {
+    @Test
+    public void whenFirstLessSecond() {
+        //assign block
+        Max maxim = new Max();
+        //act block
+        int result = maxim.max(10,2);
+        //assert block
+        assertThat(result, is(10));
+
+    }
+}
