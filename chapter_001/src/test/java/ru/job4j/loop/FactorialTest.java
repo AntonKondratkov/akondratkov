48fa8aa3d1c2639a44d127590950ebc7d8103df7
Anton Kondratkov <kondratkov.anton@yandex.ru>
2017-11-17 22:24:47 +0600

    4.2. Создать программу вычисляющую факториал. [#193]

---
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ *@author Anton Kondratkov
+ *@since 17.11.2017
+ */
+
+public class FactorialTest{
+    @Test
+     public void whenCalculateFactorialForFiveThenOneHundreedTwenty(){
+        //создаём объект.
+        Factorial sum = new Factorial();
+        //задаём число для вычисления факториала.
+        int result = sum.calc(5);
+        // Задаем ожидаемый результат.
+        int expected = 120;
+        //Проверяем результат и ожидаемое значение.
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenCalculateFactorialForZeroThenOne(){
+        //создаём объект.
+        Factorial sum = new Factorial();
+        //задаём число для вычисления факториала.
+        int result = sum.calc(0);
+        // Задаем ожидаемый результат.
+        int expected = 1;
+        //Проверяем результат и ожидаемое значение.
+        assertThat(result, is(expected));
+    }
+}
