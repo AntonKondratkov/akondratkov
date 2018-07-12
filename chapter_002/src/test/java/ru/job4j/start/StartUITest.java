3f684807ece4356ec6a94983b8d998247130c86c
AntonKondratkov <kondratkov.anton@yandex.ru>
2018-07-12 16:04:18 +1200

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль. [#33585]

--- chapter_002/src/test/java/ru/job4j/start/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/start/StartUITest.java
@@ -8,12 +8,15 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
- * @version $Id$
- * @since 0.
+ * @author Anton Kondratkov
+ * @since 12.07.18.
  * В данном классе расположены JUnit тесты, использующие класс StubInput для эмуляции поведения пользователя.
  **/
 
 public class StartUITest {
+
+    private final Tracker tracker = new Tracker();
+    private final Item item = tracker.add(new Item("name5", "desc5"));
 
     @Test
     public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
@@ -55,5 +58,36 @@ public class StartUITest {
         new StartUI(input, tracker).init();
         //Проверяем, что удалённый элемент отсутсвует и на его месте находится следующая заявка.
         assertThat(tracker.findAll()[0].getName(), is(item4.getName()));
+    }
+    @Test
+    public void whenShowItems(){
+        //Создаём StubInput с последовательностью действий(показать все заявки).
+        Input input = new StubInput(new String[]{"1", "6"});
+        //Создаём StartUI и вызываем метод init().
+        new StartUI(input, tracker).init();
+        //Проверяем, что найденные заявки соотвествуют тем, что добавили (item).
+        Item[] expect  = {item};
+        assertThat(tracker.findAll(), is(expect));
+    }
+
+    @Test
+    public void whenFindId(){
+        //Создаём StubInput с последовательностью действий(найти заявку по id).
+        Input input = new StubInput(new String[]{"4", item.getId(), "6"});
+        //Создаём StartUI и вызываем метод init().
+        new StartUI(input, tracker).init();
+        //Проверяем, что найденная по id заявка соотвествуют добавленной (item).
+        assertThat(tracker.findById(item.getId()), is(item));
+    }
+
+    @Test
+    public void whenFindName(){
+        //Создаём StubInput с последовательностью действий(найти заявку по имени).
+        Input input = new StubInput(new String[]{"5", item.getName(), "6"});
+        //Создаём StartUI и вызываем метод init().
+        new StartUI(input, tracker).init();
+        //Проверяем, что найденная по имени заявка соотвествуют добавленной (item).
+        Item[] expect = {item};
+        assertThat(tracker.findByName(item.getName()), is(expect));
     }
 }
