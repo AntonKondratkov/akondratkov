b0ea055b6daff114a60cbeea227d9d2731532b4a
AntonKondratkov <kondratkov.anton@yandex.ru>
2018-07-24 18:55:43 +1200

    1. Конвертация листа массивов в один лист Integer.

--- chapter_003/src/main/java/ru.job4j/list/ConvertMatrix2List.java
+++ chapter_003/src/main/java/ru.job4j/list/ConvertMatrix2List.java
@@ -4,10 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 /**
  * @author Anton Kondratkov
- * @since 23.07.18.
- * Класс конвертирует двумерный массив в ArrayList.
+ * @since 24.07.18.
+ * Класс содержит методы для:
+ * 1. Конвертации двумерного массив в ArrayList.
+ * 2. Конвертации листа с массивами типа int в лист содержащий элементы этих массивов.
  */
 public class ConvertMatrix2List {
+    /**
+     * Метод конвертирует двумерный массив в ArrayList.
+     * @param array - Двумерный массив.
+     * @return - ArrayList.
+     */
     public List<Integer> toList(int[][] array) {
         List<Integer> list = new ArrayList<>();
         for (int[] i : array) {
@@ -16,5 +23,20 @@ public class ConvertMatrix2List {
             }
         }
         return list;
+    }
+
+    /**
+     * Метод конвертирует лист с массивами типа int в лист содержащий элементы этих массивов.
+     * @param list - Лист с массивами типа int.
+     * @return result - Лист с элементами массивов.
+     */
+    public List<Integer> convert(List<int[]> list) {
+        List<Integer> result = new ArrayList<>();
+        for (int[] value : list) {
+            for (int value2 : value) {
+                result.add(value2);
+            }
+        }
+        return result;
     }
 }
--- chapter_003/src/test/java/ru.job4j/search/list/ConvertMatrix2ListTest.java
+++ chapter_003/src/test/java/ru.job4j/search/list/ConvertMatrix2ListTest.java
@@ -3,6 +3,7 @@ package ru.job4j.search.list;
 import org.junit.Test;
 import ru.job4j.list.ConvertMatrix2List;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,7 +11,7 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 /**
  * @author Anton Kondratkov
- * @since 23.07.18.
+ * @since 24.07.18.
  * Данный класс проверяет работу класса ConvertMatrix2List.
  */
 
@@ -28,5 +29,19 @@ public class ConvertMatrix2ListTest {
         );
         List<Integer> result = list.toList(input);
         assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenListSomeArrayToOneListIntiger() {
+        List<int[]> list = new ArrayList<>();
+        list.add(new int[]{1, 2});
+        list.add(new int[]{3, 4, 5, 6});
+        list.add(new int[]{7, 8});
+        List<Integer> result = new ConvertMatrix2List().convert(list);
+        List<Integer> expect = Arrays.asList(
+                1, 2, 3, 4, 5, 6, 7, 8
+        );
+        assertThat(result, is(expect));
+
     }
 }
