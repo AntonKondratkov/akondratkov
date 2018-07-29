c0c14ba6e840f18892e1cae1403078c1195e2bd0
AntonKondratkov <kondratkov.anton@yandex.ru>
2018-07-29 15:55:46 +1200

    5.1.2. Создать итератор четные числа

---
+++ chapter_005/src/main/java/ru.job4j/iterator/EvenIterator.java
@@ -0,0 +1,49 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+/**
+ * Class EvenIterator. Итератор возвращающий только чётный цифры.
+ * @author Anton Kondratkov
+ * @since 29.07.2018
+ */
+public class EvenIterator implements Iterator {
+
+    private final int[] array;
+    private int index = 0;
+
+    public EvenIterator(int[] array) {
+        this.array = array;
+    }
+    /**
+     * Метод возвращает true если следующее число чётное или
+     * false если следующее число нечетное.
+     * @return переменная indicator;
+     */
+    @Override
+    public boolean hasNext() {
+        boolean indicator = false;
+        for (int i = index; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                indicator = true;
+                index = i;
+                break;
+            }
+        }
+        return indicator;
+    }
+    /**
+     * Метод возвращает только чётные цифры.
+     * @return переменная number.
+     */
+    @Override
+    public Object next() {
+        int number = 0;
+        if (hasNext()) {
+            number = array[index++];
+        } else {
+            throw new NoSuchElementException();
+        }
+        return number;
+    }
+}
