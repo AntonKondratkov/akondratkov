39223ef1db0712734fb86cfd759e028c2419f34f
AntonKondratkov <kondratkov.anton@yandex.ru>
2018-08-09 15:53:06 +1200

    5.3.0 Создать метод delete для односвязного списка

---
+++ chapter_005/src/main/java/ru.job4j/iterator/List/SimpleArrayList.java
@@ -0,0 +1,70 @@
+package ru.job4j.iterator.List;
+
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+public class SimpleArrayList<e> {
+
+    private int size;
+    private Node<e> first;
+    LinkedList<e> linkedList = new LinkedList<>();
+
+    /**
+     * Метод вставляет данные в начало списка.
+     * @param data добавляемый элемент.
+     */
+    public void add(e data) {
+        Node<e> newLink = new Node<>(data);
+        newLink.next = this.first;
+        this.first = newLink;
+        this.size++;
+    }
+
+    /**
+     * Метод удаляет первый элемент в списке.
+     * @return удалённый элемент.
+     */
+    public e delete() {
+        Node<e> newFirst = this.first;
+        if (newFirst == null) {
+            throw new NoSuchElementException();
+        }
+        this.first = null;
+        size--;
+        return newFirst.date;
+    }
+
+    /**
+     * Метод возвращает элемент по индексу.
+     * @param index индекс элемента.
+     * @return элемент по заданному индексу.
+     */
+    public e get(int index) {
+        Node<e> result = this.first;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result.date;
+    }
+
+    /**
+     * Метод получения размера коллекции.
+     * @return размер коллекции.
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * Класс предназначен для хранения данных.
+     * @param <e> тип хранимых данных.
+     */
+    private static class Node<e> {
+        e date;
+        Node<e> next;
+
+        Node(e date) {
+            this.date = date;
+        }
+    }
+}
