1a68fd1e72a9c04d46bd6b9df195c0c79f6afda4
AntonKondratkov <kondratkov.anton@yandex.ru>
2018-08-28 19:03:38 +1200

    5.3.4. Задан связанный список. Определить цикличность.

--- chapter_005/src/main/java/ru.job4j/iterator/list/NodeCycle.java
+++ chapter_005/src/main/java/ru.job4j/iterator/list/NodeCycle.java
@@ -1,16 +1,39 @@
 package ru.job4j.iterator.list;
-
+/**
+ * @author Anton Kondratkov
+ * @since 28.08.18.
+ * Определение цикличности связного списка.
+ **/
 public class NodeCycle {
 
+    /**
+     * Класс элементов списка.
+     * @param <T> тип данных.
+     */
     static class Node<T> {
+        /**
+         * Значение.
+         */
         T value;
+        /**
+         * Ссылка на следующий элемент.
+         */
         Node<T> next;
 
+        /**
+         * Конструктор.
+         * @param value значение элемента.
+         */
         Node(T value) {
             this.value = value;
         }
     }
 
+    /**
+     * Метод определяет цикличность в связном списке.
+     * @param first начальный элемент.
+     * @return true - список цикличный, false - список не цикличный.
+     */
     boolean hasCycle(Node first) {
         Node tortoise = first;
         Node hare = first.next;
--- chapter_005/src/test/java/ru/job4j/iterator/list/NodeCycleTest.java
+++ chapter_005/src/test/java/ru/job4j/iterator/list/NodeCycleTest.java
@@ -5,8 +5,15 @@ import ru.job4j.iterator.list.NodeCycle.Node;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
-
+/**
+ * @author Anton Kondratkov
+ * @since 28.08.18.
+ * Тестирование класса NodeCycle.
+ **/
 public class NodeCycleTest {
+    /**
+     * Один из элементов зациклен на себя и один элемент равен null.
+     */
     @Test
     public void whenNodeEqualsNull() {
         Node first = new Node(1);
@@ -16,14 +23,37 @@ public class NodeCycleTest {
 
         first.next = two;
         two.next = third;
-        third.next = four;
+        third.next = third;
         four.next = null;
 
         NodeCycle nodeCycle = new NodeCycle();
 
-        assertThat(nodeCycle.hasCycle(first), is(false));
-        assertThat(nodeCycle.hasCycle(two), is(false));
-        assertThat(nodeCycle.hasCycle(third), is(false));
+        assertThat(nodeCycle.hasCycle(first), is(true));
+        assertThat(nodeCycle.hasCycle(two), is(true));
+        assertThat(nodeCycle.hasCycle(third), is(true));
         assertThat(nodeCycle.hasCycle(four), is(false));
+    }
+
+    /**
+     * Тест на цикличность списка.
+     */
+    @Test
+    public void whenNodeNotEqualsNull() {
+        Node first = new Node(1);
+        Node two = new Node(2);
+        Node third = new Node(3);
+        Node four = new Node(4);
+
+        first.next = two;
+        two.next = third;
+        third.next = four;
+        four.next = first;
+
+        NodeCycle nodeCycle = new NodeCycle();
+
+        assertThat(nodeCycle.hasCycle(first), is(true));
+        assertThat(nodeCycle.hasCycle(two), is(true));
+        assertThat(nodeCycle.hasCycle(third), is(true));
+        assertThat(nodeCycle.hasCycle(four), is(true));
     }
 }
