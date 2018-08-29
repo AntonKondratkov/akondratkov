0bee0431034e5ebe158a564444a237e88ad2d876
AntonKondratkov <kondratkov.anton@yandex.ru>
2018-08-30 11:40:03 +1200

    1. Реализовать коллекцию Set на массиве

--- chapter_005/src/main/java/ru.job4j/iterator/set/SimpleSet.java
+++ chapter_005/src/main/java/ru.job4j/iterator/set/SimpleSet.java
@@ -3,11 +3,18 @@ package ru.job4j.iterator.set;
 
 import ru.job4j.iterator.generic.SimpleArray;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 /**
  * Коллекция set на массиве.
  * @param <T> тип данных.
  */
-public class SimpleSet<T>  {
+public class SimpleSet<T> implements Iterable<T> {
+    /**
+     * Позиция в массиве.
+     */
+    private int index = 0;
     /**
      * Объект класса SimpleArray.
      */
@@ -19,6 +26,7 @@ public class SimpleSet<T>  {
     public void add(T model) {
         if (!array.contains(model)) {
             array.add(model);
+            index++;
         }
     }
     /**
@@ -28,5 +36,28 @@ public class SimpleSet<T>  {
      */
     public T get(int index) {
         return array.get(index);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            int inIterator = 0;
+            @Override
+            public boolean hasNext() {
+                boolean indicator = true;
+                if (inIterator >= index) {
+                    indicator = false;
+                }
+                return indicator;
+            }
+
+            @Override
+            public T next() {
+                if(!hasNext()) {
+                    throw new NoSuchElementException("Элементы не найдены");
+                }
+                return array.get(inIterator++);
+            }
+        };
     }
 }
--- chapter_005/src/test/java/ru/job4j/iterator/set/SimpleSetTest.java
+++ chapter_005/src/test/java/ru/job4j/iterator/set/SimpleSetTest.java
@@ -2,15 +2,19 @@ package ru.job4j.iterator.set;
 
 import org.junit.Test;
 
+import java.util.Iterator;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 /**
  * Проверка класса SimpleSet.
  */
 public class SimpleSetTest {
+
+    private SimpleSet<Integer> simpleSet = new SimpleSet<>();
+
     @Test
     public void whenAddToSetSameElement() {
-        SimpleSet<Integer> simpleSet = new SimpleSet<>();
         simpleSet.add(1);
         simpleSet.add(1);
 
@@ -18,5 +22,20 @@ public class SimpleSetTest {
 
         assertThat(simpleSet.get(0), is(1));
         assertThat(simpleSet.get(1), is(result));
+
+    }
+
+    @Test
+    public void checkIterator() {
+        Iterator<Integer> it = simpleSet.iterator();
+        simpleSet.add(1);
+        simpleSet.add(2);
+
+        assertThat(it.hasNext(),is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.hasNext(),is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(),is(false));
+
     }
 }
