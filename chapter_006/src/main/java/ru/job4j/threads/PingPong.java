b4a6a3e0ad99675128b66c2f3572f57c5c489333
AntonKondratkov <kondratkov.anton@yandex.ru>
2018-09-21 12:33:58 +1200

    1. Пин-понг.[#70949]

--- chapter_006/src/main/java/ru/job4j/threads/PingPong.java
+++ chapter_006/src/main/java/ru/job4j/threads/PingPong.java
@@ -20,7 +20,7 @@ public class PingPong extends Application {
         int limitX = 300;
         int limitY = 300;
         Group group = new Group();
-        Rectangle rect = new Rectangle(50, 100, 10, 10);
+        Rectangle rect = new Rectangle(0, 100, 10, 10);
         group.getChildren().add(rect);
         Thread t = new Thread(new RectangleMove(rect));
         t.start();
--- chapter_006/src/main/java/ru/job4j/threads/RectangleMove.java
+++ chapter_006/src/main/java/ru/job4j/threads/RectangleMove.java
@@ -4,7 +4,7 @@ import javafx.scene.shape.Rectangle;
 /**
  * Class RectangleMove. Задаёт логику движения квадратика.
  * @author Anton Kondratkov
- * @since 14.09.2018
+ * @since 21.09.2018
  */
 
 public class RectangleMove implements Runnable {
@@ -19,13 +19,12 @@ public class RectangleMove implements Runnable {
         try {
             while (!Thread.interrupted()) {
                 if (this.rect.getX() == 290) {
-                    for (int i = 290; i > 0; i -= 5) {
-                        this.rect.setX(this.rect.getX() - 5);
-                        Thread.sleep(50);
-                    }
+                    Thread.sleep(50);
+                    this.rect.setX(0);
+                } else {
+                    this.rect.setX(this.rect.getX() + 5);
+                    Thread.sleep(50);
                 }
-                this.rect.setX(this.rect.getX() + 5);
-                Thread.sleep(50);
             }
         }  catch (InterruptedException e) {
             System.out.println("Поток прерван");
