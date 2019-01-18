722560778267204155ec84b00b7c313a598ee313
Anton <kondratkov.anton@yandex.ru>
2019-01-19 06:47:10 +0700

    1. Реализовать шаблон Producer Consumer.

--- chapter_006/src/main/java/ru/job4j/threads/wait/SimpleBlockingQueue.java
+++ chapter_006/src/main/java/ru/job4j/threads/wait/SimpleBlockingQueue.java
@@ -10,67 +10,36 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+/**
+ * @author Anton Kondratkov
+ * @since 19.01.19.
+ * Класс реализует шаблон Producer Consumer.
+ **/
 
 @ThreadSafe
 public class SimpleBlockingQueue<T> {
     @GuardedBy("this")
     private Queue<T> queue = new LinkedList<>();
-    private final int LIMIT = 10;
+    private final int LIMIT = 2;
 
     public void offer(T value) throws InterruptedException {
-        for (int i = 0; i < 13; i++) {
-            synchronized (this) {
-                while (queue.size() == LIMIT) {
-                    System.out.println("Вызов метода wait в методе offer");
-                    wait();
-                }
-                queue.offer(value);
-                notify();
+        synchronized (this) {
+            while (queue.size() == LIMIT) {
+                wait();
             }
+            queue.offer(value);
+            notify();
         }
     }
 
-    public void poll() throws InterruptedException {
-        for (int i = 0; i < 13; i++) {
-            synchronized (this) {
-                while (queue.size() == 0) {
-                    System.out.println("Вызов метода wait в методе poll");
-                    wait();
-                }
-                System.out.println(queue.poll());
-                Thread.sleep(1000);
-                notify();
+    public T poll() throws InterruptedException {
+        Thread.sleep(1000);
+        synchronized (this) {
+            while (queue.size() == 0) {
+                wait();
             }
+            notify();
         }
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-        SimpleBlockingQueue sb = new SimpleBlockingQueue();
-        Thread producer = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    sb.offer(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        Thread consumer = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    sb.poll();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        producer.start();
-        consumer.start();
-
-        producer.join();
-        consumer.join();
+        return queue.poll();
     }
 }
---
+++ chapter_006/src/test/java/ru/job4j/synchronizy/wait/SimpleBlockingQueueTest.java
@@ -0,0 +1,86 @@
+package ru.job4j.synchronizy.wait;
+
+import org.junit.Test;
+import ru.job4j.threads.wait.SimpleBlockingQueue;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Anton Kondratkov
+ * @since 19.01.19.
+ * Тестирование класса SimpleBlockingQueue.
+ **/
+public class SimpleBlockingQueueTest {
+    /**
+     *  Данный метод проверяет добавление одного элемента в очередь и его извлечение.
+     * @throws InterruptedException
+     */
+    @Test
+    public void whenAddElementToQueue() throws InterruptedException {
+        SimpleBlockingQueue<Integer> sb = new SimpleBlockingQueue<>();
+        int[] result = new int[1];
+        Thread producer = new Thread(() -> {
+            try {
+                sb.offer(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        Thread consumer = new Thread(() -> {
+            try {
+               result[0] = sb.poll();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        consumer.join();
+
+
+        assertThat(result[0], is(1));
+    }
+    /**
+     * Данный метод проверяет добавление 5 элементов в очередь и их извлечение.
+     * @throws InterruptedException
+     */
+    @Test
+    public void whenAddSeveralElementToQueue() throws InterruptedException {
+        SimpleBlockingQueue<Integer> sb = new SimpleBlockingQueue<>();
+        int[] result = new int[5];
+        Thread producer = new Thread(() -> {
+            try {
+                for(int i = 0; i < 5; i++) {
+                    sb.offer(i);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        Thread consumer = new Thread(() -> {
+            try {
+                for(int i = 0; i < 5; i++) {
+                    result[i] = sb.poll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        consumer.join();
+
+
+        assertThat(result[0], is(0));
+        assertThat(result[1], is(1));
+        assertThat(result[2], is(2));
+        assertThat(result[3], is(3));
+        assertThat(result[4], is(4));
+    }
+}
