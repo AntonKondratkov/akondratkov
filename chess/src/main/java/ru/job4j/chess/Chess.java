a94519041065804e0b652a9f7bc454e8189b31b0
Anton <kondratkov.anton@yandex.ru>
2019-07-14 02:59:54 +0700

    Каркас шахматной доски

--- chess/src/main/java/ru/job4j/chess/Chess.java
+++ chess/src/main/java/ru/job4j/chess/Chess.java
@@ -64,11 +64,10 @@ public class Chess extends Application {
                         if (logic.move(this.findBy(momento.getX(), momento.getY()), this.findBy(event.getX(), event.getY()))) {
                             rect.setX(((int) event.getX() / 40) * 40 + 5);
                             rect.setY(((int) event.getY() / 40) * 40 + 5);
-                        } else {
-                            rect.setX(((int) momento.getX() / 40) * 40 + 5);
-                            rect.setY(((int) momento.getY() / 40) * 40 + 5);
                         }
                     } catch (ImpossibleMoveException e) {
+                        rect.setX(((int) momento.getX() / 40) * 40 + 5);
+                        rect.setY(((int) momento.getY() / 40) * 40 + 5);
                         e.printStackTrace();
                     }
                 }
