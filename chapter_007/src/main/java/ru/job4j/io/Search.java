86edb7917c3cde8c9f60f86bc49d10cf41a69dcd
Anton <kondratkov.anton@yandex.ru>
2019-07-23 11:18:31 +0700

    3. Сканирование файловой системы.

---
+++ chapter_007/src/main/java/ru/job4j/io/Search.java
@@ -0,0 +1,42 @@
+package ru.job4j.io;
+
+import org.apache.commons.io.FilenameUtils;
+
+import java.io.File;
+import java.util.*;
+/**
+ *Класс сканирует файловую систему.
+ *@author Anton Kondratkov
+ *@since 23.07.2019
+ */
+public class Search {
+    /**
+     * Метод возвращает список всех файлов с конкретным расширением в директории.
+     * @param parent - Путь до каталога, с которого нужно осуществлять поиск.
+     * @param exts - Список расширений файлов, которые необходимо получить.
+     * @return files - Список файлов.
+     */
+    List<File> files(String parent, List<String> exts) {
+        List<File> files = new ArrayList<>();
+        if (exts != null && exts.size() != 0) {
+            File file = new File(parent);
+            if (file.exists()) {
+                Queue<File> filesForCheck = new LinkedList<>();
+                filesForCheck.offer(file);
+                while (!filesForCheck.isEmpty()) {
+                    File[] currentFiles = filesForCheck.poll().listFiles();
+                    if (currentFiles != null) {
+                        for (File f : currentFiles) {
+                            if (f.isDirectory()) {
+                                filesForCheck.offer(f);
+                            } else if (exts.contains(FilenameUtils.getExtension(f.getName()))) {
+                                files.add(f);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return files;
+    }
+}
