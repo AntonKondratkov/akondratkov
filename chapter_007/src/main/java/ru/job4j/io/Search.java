ded9887777905319d25d0a8d174afcba6eb252bd
Anton <kondratkov.anton@yandex.ru>
2019-07-25 10:37:43 +0700

    3. Сканирование файловой системы.

--- chapter_007/src/main/java/ru/job4j/io/Search.java
+++ chapter_007/src/main/java/ru/job4j/io/Search.java
@@ -7,7 +7,7 @@ import java.util.*;
 /**
  *Класс сканирует файловую систему.
  *@author Anton Kondratkov
- *@since 23.07.2019
+ *@since 25.07.2019
  */
 public class Search {
     /**
@@ -23,16 +23,27 @@ public class Search {
             if (file.exists()) {
                 Queue<File> filesForCheck = new LinkedList<>();
                 filesForCheck.offer(file);
-                while (!filesForCheck.isEmpty()) {
-                    File[] currentFiles = filesForCheck.poll().listFiles();
-                    if (currentFiles != null) {
-                        for (File f : currentFiles) {
-                            if (f.isDirectory()) {
-                                filesForCheck.offer(f);
-                            } else if (exts.contains(FilenameUtils.getExtension(f.getName()))) {
-                                files.add(f);
-                            }
-                        }
+                files = checkingExtension(exts, filesForCheck);
+            }
+        }
+        return files;
+    }
+    /**
+     * Метод ищет в списке файлов, файлы с нужным расширением.
+     * @param exts - Список с расширениями файлов.
+     * @param filesForCheck - Файлы для проверки.
+     * @return files - Список файлов с нужными расширениями.
+     */
+    public List<File> checkingExtension(List<String> exts, Queue<File> filesForCheck) {
+        List<File> files = new ArrayList<>();
+        while (!filesForCheck.isEmpty()) {
+            File[] currentFiles = filesForCheck.poll().listFiles();
+            if (currentFiles != null) {
+                for (File f : currentFiles) {
+                    if (f.isDirectory()) {
+                        filesForCheck.offer(f);
+                    } else if (exts.contains(FilenameUtils.getExtension(f.getName()))) {
+                        files.add(f);
                     }
                 }
             }
