55fc19fec0fc534dff8e222e5086ca2cab382c4e
Anton <kondratkov.anton@yandex.ru>
2019-08-09 04:33:37 +0700

    1. Бот

---
+++ chapter_007/src/main/java/ru/job4j/io/bot/Client.java
@@ -0,0 +1,53 @@
+package ru.job4j.io.bot;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+/**
+ *Клиентская сторона бота "Оракл".
+ *@author Anton Kondratkov
+ *@since 09.08.2019
+ */
+public class Client {
+    private final Socket socket;
+
+    public Client(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void start() throws IOException {
+        try (PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+             Scanner console = new Scanner(System.in)) {
+            boolean working = true;
+            String ask;
+            String answer;
+            do {
+                ask = console.nextLine();
+                out.println(ask);
+                answer = in.readLine();
+                while (!answer.isEmpty()) {
+                    System.out.println(answer);
+                    answer = in.readLine();
+                }
+                if (ask.equals("exit")) {
+                    working = false;
+                }
+            } while (working);
+            System.out.println("Shutdown chat!");
+        }
+    }
+
+    public static void main(String[] args) {
+        try (Socket socket = new Socket(InetAddress.getByName("127.0.0.1"), 1111)) {
+            Client client = new Client(socket);
+            client.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ chapter_007/src/main/java/ru/job4j/io/bot/Server.java
@@ -0,0 +1,68 @@
+package ru.job4j.io.bot;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+/**
+ *Серверная сторона бота "Оракл".
+ *@author Anton Kondratkov
+ *@since 09.08.2019
+ */
+public class Server {
+    private final Socket socket;
+
+    /**
+     * Коллекция с ответами.
+     */
+    private final Map<String, String> digest = new HashMap<>();
+
+    public Server(Socket socket) {
+        this.socket = socket;
+        init();
+    }
+
+    public void start() throws IOException {
+        try (PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()))) {
+            boolean working = true;
+            String ask;
+            String answer;
+            do {
+                System.out.println("wait command ...");
+                ask = in.readLine();
+                System.out.println(ask);
+                answer = digest.get(ask);
+                if  (answer == null) {
+                    answer = "Random phrase.";
+                }
+                if ("exit".equals(ask)) {
+                    working = false;
+                }
+                out.println(answer);
+                out.println();
+            } while (working);
+            System.out.println("Shutdown server");
+        }
+    }
+    /**
+     * Метод добавляет в коллекцию digest пару вопрос - ответ.
+     */
+    private void init() {
+        this.digest.put("hello", "Hello, dear friend, I'm a oracle.");
+        this.digest.put("exit", "GoodBye, my friend, let the force be with you!");
+    }
+
+    public static void main(String[] args) {
+        try (Socket socket = new ServerSocket(1111).accept()) {
+            Server server = new Server(socket);
+            server.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
