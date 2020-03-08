034c0e26db393366f0e9b4b815ed0e291f3c9361
Anton <kondratkov.anton@yandex.ru>
2020-03-09 03:28:49 +0700

    Реализации кеша на SoftReference (add comments)

--- chapter_010/src/main/java/ru/job4j/references/Cash.java
+++ chapter_010/src/main/java/ru/job4j/references/Cash.java
@@ -6,42 +6,64 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
-
+/**
+ * @author Anton Kondratkov
+ * @since 9.03.2020
+ * Класс реализует структуру данных типа кеш на ссылках SoftReference
+ */
 public class Cash {
-    private Map<String, SoftReference<String>> cashMap = new HashMap<>();
-
+    public Map<String, SoftReference<String>> cashMap = new HashMap<>();
+    /**
+     * Метод добавляет объект в кеш
+     * @param key - ключ объекта
+     * @param text - содержимое объекта
+     * @return содержимое объекта
+     */
     private Object add(String key, String text) {
         SoftReference softReference = cashMap.put(key, new SoftReference<>(text));
-        if(softReference == null) {
+        if (softReference == null) {
             return null;
         }
         return softReference.get();
     }
-
-    public Object get (String key) {
+    /**
+     * Метод получает объект из кеша
+     * @param key - ключ объекта
+     * @return содержимое объекта
+     */
+    public Object get(String key) {
         SoftReference softReference = cashMap.get(key);
-        if(softReference == null) {
+        if (softReference == null) {
             return null;
         }
         return softReference.get();
     }
-
+    /**
+     * Метод удаляет объект из кеша
+     * @param key - ключ объекта
+     * @return содержимое объекта
+     */
     public Object remove(String key) {
         SoftReference softReference = cashMap.remove(key);
-        if(softReference == null) {
+        if (softReference == null) {
             return null;
         }
         return softReference.get();
     }
-
+    /**
+     * Метод получает объект из кеша.
+     * Если объекта в кеше нет, добавляет его в кеш
+     * @param key - ключ объекта
+     * @return содержимое объекта
+     * @throws IOException
+     */
     public Object text(String key) throws IOException {
         String result = null;
-
-        if(get(key) == null) {
+        if (get(key) == null) {
             result = Files.lines(Paths.get(key)).reduce("", String::concat);
             add(key, result);
         } else {
-            result = (String)get(key);
+            result = (String) get(key);
         }
         return result;
     }
