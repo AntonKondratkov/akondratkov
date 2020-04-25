ea20576c160ef7c9e0bf7430dde4a1182ef17e30
Anton <kondratkov.anton@yandex.ru>
2020-04-25 21:20:49 +0700

    0.2. FileInputStream (two options: with BufferedReader and FileInputStream)

--- chapter_007/src/main/java/ru/job4j/io/stream/EvenNumberFile.java
+++ chapter_007/src/main/java/ru/job4j/io/stream/EvenNumberFile.java
@@ -1,7 +1,7 @@
 package ru.job4j.io.stream;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.FileInputStream;
+
 /**
  *Класс построчно считывает числа из файла и проверяет их на чётность.
  *@author Anton Kondratkov
@@ -9,11 +9,26 @@ import java.io.FileReader;
  */
 public class EvenNumberFile {
     public static void main(String[] args) {
-        try (BufferedReader in = new BufferedReader(new FileReader("even.txt"))) {
-            String line;
-            while ((line = in.readLine()) != null) {
-                System.out.println(Integer.valueOf(line) % 2 == 0);
+//        try (BufferedReader in = new BufferedReader(new FileReader("even.txt"))) {
+//            String line;
+//            while ((line = in.readLine()) != null) {
+//                System.out.println(Integer.valueOf(line) % 2 == 0);
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+        try (FileInputStream in = new FileInputStream("even.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
             }
+            String [] strings = text.toString().split("\n");
+            for (String s : strings) {
+                System.out.println(Long.valueOf(s.trim()) % 2 == 0);
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
