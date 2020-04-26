9598a0fee4665d925d65dc7461be33c0a3368c80
Anton <kondratkov.anton@yandex.ru>
2020-04-26 22:02:59 +0700

    3.0. Тестирование IO

--- chapter_007/src/test/java/ru/job4j/io/ConfigTest.java
+++ chapter_007/src/test/java/ru/job4j/io/ConfigTest.java
@@ -1,8 +1,9 @@
 package ru.job4j.io;
 
-import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -10,20 +11,23 @@ import java.io.IOException;
 import java.io.Writer;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 /**
- *Данный класс тестирует работу класса Config.
+ *Класс тестирует работу класса Config.
+ *В данном тесте используется класс TemporaryFolder. Он позволяет создавать файлы и директории во временном каталоге.
+ *После запуска теста, файлы созданные через TemporaryFolder будут удалены.
  *@author Anton Kondratkov
- *@since 20.07.2019
+ *@since 26.04.2020
  */
 public class ConfigTest {
+    private File file;
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Before
     public void createFile() throws IOException {
-        File file = new File("test.properties");
-        if (!file.exists()) {
-            file.createNewFile();
-        }
+        file = folder.newFile("test.properties");
         String ls = System.lineSeparator();
         String data =
                 "## Test properties file for [#858]" + ls
@@ -35,23 +39,16 @@ public class ConfigTest {
                         + "       prop2         =            value2         " + ls
                         + "     ## Test coment string" + ls
                         + " prop3.prop3= value3" + ls;
-        Writer writer = new FileWriter(file);
-        writer.append(data);
-        writer.flush();
-        writer.close();
-    }
-
-    @After
-    public void deleteFile() {
-        File file = new File("test.properties");
-        if (file.exists()) {
-            file.delete();
+        try (Writer writer = new FileWriter(file)) {
+            writer.append(data);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     @Test
     public void whenLoadThenConfigHasProperties() {
-        Config config = new Config("test.properties");
+        Config config = new Config(file.getAbsolutePath());
         assertThat(config.getValues().size(), is(0));
         config.load();
         assertThat(config.getValues().size(), is(3));
@@ -59,7 +56,7 @@ public class ConfigTest {
 
     @Test
     public void whenInvokeValueThenGetIt() {
-        Config config = new Config("test.properties");
+        Config config = new Config(file.getAbsolutePath());
         config.load();
         assertThat(config.value("prop1"), is("value1"));
         assertThat(config.value("prop2"), is("value2"));
