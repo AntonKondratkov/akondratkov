f548e84c5e03ec1a44c0e75a0cc14ebe4bf3de02
Anton <kondratkov.anton@yandex.ru>
2020-04-26 21:04:43 +0700

    0.4. BufferedOutputStream

--- chapter_007/src/main/java/ru/job4j/io/stream/LogFilter.java
+++ chapter_007/src/main/java/ru/job4j/io/stream/LogFilter.java
@@ -1,13 +1,11 @@
 package ru.job4j.io.stream;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Класс содержит метод filter в котором происходит построчное считывание файла
- * и возврат строки, где предпоследнее число - это 404.
+ * Класс содержит методы для файлового чтения и записи данных.
  * @author Anton Kondratkov
  * @since 26.04.2020
  */
@@ -19,15 +17,28 @@ public class LogFilter {
      */
     public static List<String> filter(String file) {
         List<String> lines = new ArrayList<>();
-        try (BufferedReader in = new BufferedReader(new FileReader("log.txt"))) {
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
             in.lines().filter(s -> s.matches(".+404\\s{1}\\d*")).forEach(lines::add);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return lines;
     }
+
+    /**
+     * Метод записывает данные в файл "result.txt"
+     * @param log Список отфильтрованных строк из метода filter.
+     * @param file Файл, в который происходит запись данных.
+     */
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(file)))) {
+            log.forEach(s -> out.write(s + "\n"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
-        System.out.println(log);
+        save(log, "result.txt");
     }
 }
