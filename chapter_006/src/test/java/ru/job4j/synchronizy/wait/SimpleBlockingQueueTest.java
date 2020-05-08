581a434320a1dc454f3408e11df49af4b94565cf
Anton <kondratkov.anton@yandex.ru>
2020-05-08 17:05:46 +0700

    3. Junit тест для блокирующей очереди.

--- chapter_006/src/test/java/ru/job4j/synchronizy/wait/SimpleBlockingQueueTest.java
+++ chapter_006/src/test/java/ru/job4j/synchronizy/wait/SimpleBlockingQueueTest.java
@@ -2,85 +2,46 @@ package ru.job4j.synchronizy.wait;
 
 import org.junit.Test;
 import ru.job4j.threads.wait.SimpleBlockingQueue;
+
+import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
  * @author Anton Kondratkov
- * @since 19.01.19.
+ * @since 08.05.20.
  * Тестирование класса SimpleBlockingQueue.
  **/
 public class SimpleBlockingQueueTest {
-    /**
-     *  Данный метод проверяет добавление одного элемента в очередь и его извлечение.
-     * @throws InterruptedException
-     */
-    @Test
-    public void whenAddElementToQueue() throws InterruptedException {
-        SimpleBlockingQueue<Integer> sb = new SimpleBlockingQueue<>();
-        int[] result = new int[1];
-        Thread producer = new Thread(() -> {
-            try {
-                sb.offer(1);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-        Thread consumer = new Thread(() -> {
-            try {
-               result[0] = sb.poll();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-
-        producer.start();
-        consumer.start();
-
-        producer.join();
-        consumer.join();
-
-
-        assertThat(result[0], is(1));
-    }
-    /**
+      /**
      * Данный метод проверяет добавление 5 элементов в очередь и их извлечение.
-     * @throws InterruptedException
      */
     @Test
     public void whenAddSeveralElementToQueue() throws InterruptedException {
-        SimpleBlockingQueue<Integer> sb = new SimpleBlockingQueue<>();
-        int[] result = new int[5];
-        Thread producer = new Thread(() -> {
-            try {
-                for (int i = 0; i < 5; i++) {
-                    sb.offer(i);
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-        Thread consumer = new Thread(() -> {
-            try {
-                for (int i = 0; i < 5; i++) {
-                    result[i] = sb.poll();
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
 
+        Thread producer = new Thread(() -> IntStream.range(0, 5).forEach(queue::offer));
         producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (queue.size() != 0 || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
         consumer.start();
-
         producer.join();
+        consumer.interrupt();
         consumer.join();
 
-
-        assertThat(result[0], is(0));
-        assertThat(result[1], is(1));
-        assertThat(result[2], is(2));
-        assertThat(result[3], is(3));
-        assertThat(result[4], is(4));
+        assertThat(buffer, is(Arrays.asList(0, 1, 2, 3, 4)));
     }
 }
