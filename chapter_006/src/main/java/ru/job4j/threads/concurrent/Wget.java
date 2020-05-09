74ee458efdc9ccac6514103e9a05dc6eb8d22397
Anton <kondratkov.anton@yandex.ru>
2020-05-09 17:37:54 +0700

    1.2. Режим ожидания.

---
+++ chapter_006/src/main/java/ru/job4j/threads/concurrent/Wget.java
@@ -0,0 +1,30 @@
+package ru.job4j.threads.concurrent;
+/**
+ * Класс содержит метод main, который симулирует процесс загрузки.
+ * @author Anton Kondratkov
+ * @since 09.05.2020
+ */
+public class Wget {
+    /**
+     * Метод print печатает символы в строку без перевода каретки.
+     * Символ \r указывает, что каретку каждый раз нужно вернуть в начало строки.
+     * Это позволяет через промежуток времени обновить строчку
+     * @param args args
+     */
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {
+                    for (int i = 0; i < 100; i++) {
+                        try {
+                            Thread.sleep(1000);
+                            System.out.print("\rLoading : " + i + "%");
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+        );
+        first.start();
+    }
+}
